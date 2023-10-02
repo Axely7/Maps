61941e026a6ac71c1d13485e62c745356b9bbd51
Axely7 <axely7@hotmail.com>
2023-10-02 12:20:55 -0600

    setting markers

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,15 +1,55 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { PlacesContext, MapContext } from "../context";
+import { LoadingPlaces } from "./";
+import { Feature } from "../interfaces/places";
 
 export const SearchResults = () => {
+  const { isLoadingPlaces, places } = useContext(PlacesContext);
+  const { map } = useContext(MapContext);
+
+  const [activeId, setActiveId] = useState("");
+
+  const onPlaceClicked = (place: Feature) => {
+    setActiveId(place.id);
+
+    const [lng, lat] = place.center;
+
+    map?.flyTo({ zoom: 14, center: [lng, lat] });
+  };
+
+  if (isLoadingPlaces) {
+    return <LoadingPlaces />;
+  }
+
+  if (places.length === 0) {
+    return <></>;
+  }
+
   return (
     <ul className="list-group mt-3">
-      <li className="list-group-item list-group-item-action">
-        <h6>Nombre del Lugar</h6>
-        <p className="text-muted" style={{ fontSize: "12px" }}>
-          In cupidatat dolor exercitation pariatur.
-        </p>
-        <button className="btn btn-outline-primary btn-sm">Direcciones</button>
-      </li>
+      {places.map((place) => (
+        <li
+          key={place.id}
+          className={`${
+            activeId === place.id ? "active" : ""
+          } list-group-item list-group-item-action pointer`}
+          onClick={() => onPlaceClicked(place)}
+        >
+          <h6>{place.text_es}</h6>
+          <p className="text-muted" style={{ fontSize: "12px" }}>
+            {place.place_name_es}
+          </p>
+          <button
+            className={`${
+              activeId === place.id
+                ? "btn-outline-light"
+                : "btn-outline-primary"
+            } btn  btn-sm`}
+          >
+            Direcciones
+          </button>
+        </li>
+      ))}
     </ul>
   );
 };
--- src/context/map/mapReducer.ts
+++ src/context/map/mapReducer.ts
@@ -1,8 +1,10 @@
-import { Map } from "mapbox-gl";
+import { Map, Marker } from "mapbox-gl";
 import { MapState } from "./MapProvider";
 
 
-type MapAction = {type: "setMap", payload: Map}
+type MapAction = 
+| {type: "setMap", payload: Map}
+| {type: "setMarkers", payload: Marker[]}
 
 
 
@@ -14,6 +16,12 @@ export const mapReducer = (state: MapState, action: MapAction): MapState => {
                 ...state,
                 isMapReady: true,
                 map: action.payload
+            }
+
+        case "setMarkers":
+            return {
+                ...state,
+                markers: action.payload
             }
     
         default:
